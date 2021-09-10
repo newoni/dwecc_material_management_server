a647a7cf50d03123fe69e3cce4be5793dc1bf439
newoni <newoni_@naver.com>
2021-09-10 11:02:01 +0900

    change BomController <21.09.10>

--- src/main/java/com/example/dwecc_server/controller/BomSearchController.java
+++ src/main/java/com/example/dwecc_server/controller/BomSearchController.java
@@ -1,25 +1,31 @@
 package com.example.dwecc_server.controller;
 
-import com.example.dwecc_server.model.entity.Product;
 import com.example.dwecc_server.model.request.BomSearchRequest;
 import com.example.dwecc_server.model.response.ProductListResponse;
 import com.example.dwecc_server.service.BomSearchService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/bomSearch")
+@Slf4j
 public class BomSearchController {
     @Autowired
     BomSearchService bomSearchService;
 
     @RequestMapping("/product")
     public ProductListResponse getProductList(@RequestBody BomSearchRequest request){
+
+        System.out.println("getProductList is invoked");
+        System.out.println("request value: " + request);
+        System.out.println("request product value: " + request.getProduct());
+        System.out.println("request model value: " + request.getModel());
+        System.out.println("request spec value: " + request.getSpec());
+
         return bomSearchService.findProductByName(request);
     }
 
