ecf6146f70675515cd0289c65d075f7c51212c08
newoni <newoni_@naver.com>
2021-09-27 08:57:22 +0900

    add comment <21.09.27>

--- src/main/java/com/example/dwecc_server/controller/BomSearchController.java
+++ src/main/java/com/example/dwecc_server/controller/BomSearchController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+// BOM Search 기능
 @RestController
 @RequestMapping("/bomSearch")
 @Slf4j
@@ -18,6 +18,12 @@ public class BomSearchController {
     @Autowired
     BomSearchService bomSearchService;
 
+/*
+BOM Search 기능(모델명 검색 데이터)
+description: 제품명 검색 후 해당 모델 리스트 출력
+input: BomSearchRequest, (String)BomSearchRequest.getProduct -> 필수
+output: ProductListResponse, 이후 (ArrayList<String>)ProductListResponse.productArrayList-> 필수
+*/
     @RequestMapping("/product")
     public ProductListResponse getProductList(@RequestBody BomSearchRequest request){
 
@@ -30,6 +36,12 @@ public class BomSearchController {
         return bomSearchService.findProductByName(request);
     }
 
+/*
+BOM Search 기능(모델 선택 후 자재리스트 출력)
+description: product, model, spec 데이터 input 투입 후 그에 대한 자재 데이터를 ArrayList<Material> type으로 리턴.
+input: BomSearchReques, BomSearchRequest's all field
+output: MaterialListResponse, (ArrayList<Material>)MaterialListResponse.getMaterialArrayList
+*/
     @RequestMapping("/material")
     public MaterialListResponse getMaterialList(@RequestBody BomSearchRequest request){
 
--- src/main/java/com/example/dwecc_server/controller/MaterialManagementController.java
+++ src/main/java/com/example/dwecc_server/controller/MaterialManagementController.java
@@ -9,24 +9,38 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
+//    창고 자재 관리 관련 기능(입/출고, 재고 확인)
 @RestController
 @RequestMapping("/material")
 public class MaterialManagementController {
     @Autowired
     LineMaterialService lineMaterialService;
 
+/*
+    description: 전체 자재 리스트 출력 버튼 클릭시 동작
+    input: none
+    output: LineMaterialListResponse, (ArrayList<LineMaterialResponse>) LineMaterialService.getLineMaterialResponseArrayList 값을 받아올 것.
+ */
     @RequestMapping("/readAll")
     public LineMaterialListResponse readAllLineMaterial(){
         return lineMaterialService.readAllLineMaterial();
     }
 
+/*
+    description: QR코드 데이터 입력을 DB에 저장
+    input: MaterialRequest, 이후 (String)code, (String)lot, (Long)qty, (Long)seq 가 필수적으로 활용됨
+    output: None
+ */
     @RequestMapping("/create")
     public void create(@RequestBody MaterialRequest request){
         lineMaterialService.create(request);
     }
 
+/*
+    description: QR코드 데이터 입력을 DB에 대조하고, 없을 경우 삭제
+    input: MaterialRequest, 이후 (String)code, (String)lot, (Long)qty, (Long)seq 가 필수적으로 활용됨
+    output: None
+ */
     @RequestMapping("/delete")
     public void delete(@RequestBody MaterialRequest request){
         lineMaterialService.delete(request);
