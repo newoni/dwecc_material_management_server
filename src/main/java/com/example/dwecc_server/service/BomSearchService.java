e446857d90c9b3d49752afc198a2766953102f04
newoni <newoni_@naver.com>
2021-09-27 10:05:55 +0900

    add comment to BomSearchService <21.09.27>

--- src/main/java/com/example/dwecc_server/service/BomSearchService.java
+++ src/main/java/com/example/dwecc_server/service/BomSearchService.java
@@ -25,21 +25,28 @@ public class BomSearchService {
     ProductRepository productRepository;
     @Autowired
     LinematerialRepository linematerialRepository;
-
     @Autowired
     MaterialRepository materialRepository;
-
     @Autowired
     RelationRepository relationRepository;
 
+
+/*
+description: 제품명을 입력 받고 해당하는 productList 출력
+input: BomSearchRequest, 이 후 BomSearchRequest.getProduct() 필요
+output: ProductListResponse, 이후 product, model, spec 활용
+*/
     public ProductListResponse findProductByName(BomSearchRequest request){
 
+//        결과값 리턴을 위한 객체 생성
         ProductListResponse productListResponse = new ProductListResponse();
         ArrayList<ProductResponse> result = new ArrayList<ProductResponse>();
+
+//        repository에서 해당 값을 List 타입으로 모두 검색
         List<Product> productList = productRepository.findByProduct(request.getProduct());
 
+//        반복문을 돌며 List<Product> -> ArrayList<ProductResponse> 로 형변환
         for(int i=0; i<productList.size(); i++){
-
             ProductResponse productResponse = new ProductResponse();
             productResponse.setProduct(productList.get(i).getProduct());
             productResponse.setModel(productList.get(i).getModel());
@@ -47,29 +54,39 @@ public class BomSearchService {
 
             result.add(productResponse);
         }
+
+//        형변환한 데이터를 출력용 객체에 추가
         productListResponse.setProductArrayList(result);
 
         log.info("BomSearchService/productListResponse: "+ productListResponse);
         return productListResponse;
     }
 
+/*
+description: product, model, spec 정보를 받아와서, 해당하는 자재 데이터 돌려줌
+input: BomSearchRequest, 이 후 product, model, spec field 필수
+output: MaterialListResponse
+*/
     public MaterialListResponse findMaterialsByName(BomSearchRequest request){
         log.info("request product: " + request.getProduct());
         log.info("request model: " + request.getModel());
         log.info("request spec: " + request.getSpec());
+
+        //결과 출력용 객체 생성
+        MaterialListResponse materialListResponse = new MaterialListResponse();
+        ArrayList<Material> materialArrayList = new ArrayList<Material>();
+
         //검색 값에 맞는 product 구하기
         Product resProduct = productRepository.findByProductAndModelAndSpec(request.getProduct(), request.getModel(), request.getSpec());
 
-        //product에 해당하는 ID 값으로 material2product 값을 List 형태로 받아오기
+        //product에 해당하는 ID 값으로 관련 자제 데이터 List 형태로 받아오기
         log.info("resProduct: " + resProduct);
         List<Relation> relationList = relationRepository.findByProduct(resProduct.getId());
 
         log.info("resRelation: " + relationList);
         log.info("resRelation size: " + relationList.size());
-        //결과 출력용 MaterialListResponse와 Material ArrayList(file값) 만들기
-        MaterialListResponse materialListResponse = new MaterialListResponse();
-        ArrayList<Material> materialArrayList = new ArrayList<Material>();
 
+//        for 문을 돌며 자재/제품 관계에서 해당 자재 정보 추출
         for(int i=0; i<relationList.size(); i++){
             materialArrayList.add(materialRepository.findById(relationList.get(i).getMaterial()).get());
         }
